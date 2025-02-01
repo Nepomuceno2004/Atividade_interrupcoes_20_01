2481b61796c1aa2c790ba92dd170273d9103497d
Matheus Nepomuceno Souza <mnepomuceno2004@gmail.com>
2025-02-01 01:18:29 -0300

    Update interrupcao_20_01.c

--- interrupcao_20_01.c
+++ interrupcao_20_01.c
@@ -15,91 +15,270 @@
 static volatile uint32_t last_time = 0; // Armazena o tempo do último evento (em microssegundos)
 
 // Variável global para armazenar a cor (Entre 0 e 255 para intensidade)
-uint8_t led_r = 0;  // Intensidade do vermelho
-uint8_t led_g = 0;  // Intensidade do verde
+uint8_t led_r = 0; // Intensidade do vermelho
+uint8_t led_g = 0; // Intensidade do verde
 uint8_t led_b = 2; // Intensidade do azul
 
 int numero_exibido = 0;
 
 // Buffer para armazenar quais LEDs estão ligados matriz 5x5
 bool numero_0[NUM_PIXELS] = {
-    1,1,1,1,1,
-    1,0,0,0,1,
-    1,0,0,0,1,
-    1,0,0,0,1,
-    1,1,1,1,1
-};
+    1, 1, 1, 1, 1,
+    1, 0, 0, 0, 1,
+    1, 0, 0, 0, 1,
+    1, 0, 0, 0, 1,
+    1, 1, 1, 1, 1};
 
 bool numero_1[NUM_PIXELS] = {
-    0,0,1,0,0,
-    0,0,1,0,0,
-    0,0,1,0,0,
-    0,0,1,0,0,
-    0,0,1,0,0,
+    0,
+    0,
+    1,
+    0,
+    0,
+    0,
+    0,
+    1,
+    0,
+    0,
+    0,
+    0,
+    1,
+    0,
+    0,
+    0,
+    0,
+    1,
+    0,
+    0,
+    0,
+    0,
+    1,
+    0,
+    0,
 };
 
 bool numero_2[NUM_PIXELS] = {
-    1,1,1,1,1,
-    1,0,0,0,0,
-    1,1,1,1,1,
-    0,0,0,0,1,
-    1,1,1,1,1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
 };
 
 bool numero_3[NUM_PIXELS] = {
-    1,1,1,1,1,
-    0,0,0,0,1,
-    1,1,1,1,1,
-    0,0,0,0,1,
-    1,1,1,1,1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
 };
 
 bool numero_4[NUM_PIXELS] = {
-    1,0,0,0,0,
-    0,0,0,0,1,
-    1,1,1,1,1,
-    1,0,0,0,1,
-    1,0,0,0,1,
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    1,
+    1,
+    0,
+    0,
+    0,
+    1,
 };
 
 bool numero_5[NUM_PIXELS] = {
-    1,1,1,1,1,
-    0,0,0,0,1,
-    1,1,1,1,1,
-    1,0,0,0,0,
-    1,1,1,1,1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
 };
 
 bool numero_6[NUM_PIXELS] = {
-    1,1,1,1,1,
-    1,0,0,0,1,
-    1,1,1,1,1,
-    1,0,0,0,0,
-    1,1,1,1,1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
 };
 
 bool numero_7[NUM_PIXELS] = {
-    0,0,0,1,0,
-    0,0,1,0,0,
-    0,1,0,0,0,
-    0,0,0,0,1,
-    1,1,1,1,1,
+    0,
+    0,
+    0,
+    1,
+    0,
+    0,
+    0,
+    1,
+    0,
+    0,
+    0,
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
 };
 
 bool numero_8[NUM_PIXELS] = {
-    1,1,1,1,1,
-    1,0,0,0,1,
-    1,1,1,1,1,
-    1,0,0,0,1,
-    1,1,1,1,1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
 };
 
 bool numero_9[NUM_PIXELS] = {
-    1,1,1,1,1,
-    0,0,0,0,1,
-    1,1,1,1,1,
-    1,0,0,0,1,
-    1,1,1,1,1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
 };
 
 static inline void put_pixel(uint32_t pixel_grb)
@@ -131,36 +310,53 @@ void set_one_led(uint8_t r, uint8_t g, uint8_t b, bool led_buffer[])
     }
 }
 
-// Função de interrupção com debouncing
-void gpio_irq_handler_A(uint gpio, uint32_t events)
+bool *escolha_desenho(int num)
 {
-    // Obtém o tempo atual em microssegundos
-    uint32_t current_time = to_us_since_boot(get_absolute_time());
-    // Verifica se passou tempo suficiente desde o último evento
-    if (current_time - last_time > 200000) // 200 ms de debouncing
+    switch (num)
     {
-        last_time = current_time; // Atualiza o tempo do último evento
-        if(numero_exibido < 9){
-            numero_exibido++;
-            
-        }
-        //set_one_led(led_r, led_g, led_b);
+    case 0:
+        return numero_0;
+    case 1:
+        return numero_1;
+    case 2:
+        return numero_2;
+    case 3:
+        return numero_3;
+    case 4:
+        return numero_4;
+    case 5:
+        return numero_5;
+    case 6:
+        return numero_6;
+    case 7:
+        return numero_7;
+    case 8:
+        return numero_8;
+    case 9:
+        return numero_9;
+    default:
+        return NULL;
     }
 }
 
-void gpio_irq_handler_B(uint gpio, uint32_t events)
+void gpio_irq_handler(uint gpio, uint32_t events)
 {
-    // Obtém o tempo atual em microssegundos
     uint32_t current_time = to_us_since_boot(get_absolute_time());
-    // Verifica se passou tempo suficiente desde o último evento
-    if (current_time - last_time > 200000) // 200 ms de debouncing
-    {
-        last_time = current_time; // Atualiza o tempo do último evento
-        if(numero_exibido > 0){
-            numero_exibido--;
 
+    if (current_time - last_time > 200000) // 200ms de debounce
+    {
+        last_time = current_time;
+
+        if (gpio == button_A && numero_exibido < 9)
+        {
+            numero_exibido++;
         }
-        //set_one_led(led_r, led_g, led_b);
+        else if (gpio == button_B && numero_exibido > 0)
+        {
+            numero_exibido--;
+        }
+
+        set_one_led(led_r, led_g, led_b, escolha_desenho(numero_exibido));
     }
 }
 
@@ -185,8 +381,8 @@ int main()
 
     ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
 
-    gpio_set_irq_enabled_with_callback(button_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler_A);
-    gpio_set_irq_enabled_with_callback(button_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler_B);
+    gpio_set_irq_enabled_with_callback(button_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(button_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
     while (1)
     {
@@ -195,27 +391,6 @@ int main()
 
         gpio_put(pinLed, false);
         sleep_ms(tempo);
-
-        /*set_one_led(led_r, led_g, led_b, numero_0);
-        sleep_ms(1000);        
-        set_one_led(led_r, led_g, led_b, numero_1);
-        sleep_ms(1000);        
-        set_one_led(led_r, led_g, led_b, numero_2);
-        sleep_ms(1000);        
-        set_one_led(led_r, led_g, led_b, numero_3);
-        sleep_ms(1000);
-        set_one_led(led_r, led_g, led_b, numero_4);
-        sleep_ms(1000);
-        set_one_led(led_r, led_g, led_b, numero_5);
-        sleep_ms(1000);
-        set_one_led(led_r, led_g, led_b, numero_6);
-        sleep_ms(1000);
-        set_one_led(led_r, led_g, led_b, numero_7);
-        sleep_ms(1000);
-        set_one_led(led_r, led_g, led_b, numero_8);
-        sleep_ms(1000);
-        set_one_led(led_r, led_g, led_b, numero_9);
-        sleep_ms(1000);*/
     }
 
     return 0;
